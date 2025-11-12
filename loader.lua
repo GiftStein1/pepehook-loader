774f9fbb01339d7322625f3d3c7085b9abd4cf8b
Stein <73061040+GiftStein1@users.noreply.github.com>
2025-11-12 14:23:25 +0100

    Rivlas

--- loader.lua
+++ loader.lua
@@ -19,6 +19,7 @@ end
 local PlaceIds = {
     [11156779721] = "https://api.luarmor.net/files/v3/loaders/fd060e320f251948f3e8cc0d7f2561ac.lua",
     [13772394625] = "https://api.luarmor.net/files/v3/loaders/aa010b7dc1a34c1b1c00bd094666d1e3.lua",
+    [17625359962] = "https://api.luarmor.net/files/v3/loaders/4cc022ded41f6816ba0f7fb8c6dda44f.lua",
     
 }
 
