01ce52dce42647bd2d527f1cc2d13ab3cbfc525d
Stein <73061040+GiftStein1@users.noreply.github.com>
2025-12-17 22:17:48 +0100

    Remove specific loader URLs from PlaceIds
    Removed two loader URLs from the PlaceIds table.

--- loader.lua
+++ loader.lua
@@ -17,10 +17,7 @@ else
     warn("⚠ This executor (" .. tostring(name) .. ") should be working, but if you experience issues, please contact support for help.")
 end
 local PlaceIds = {
-    [11156779721] = "https://api.luarmor.net/files/v3/loaders/fd060e320f251948f3e8cc0d7f2561ac.lua",
-    [13772394625] = "https://api.luarmor.net/files/v3/loaders/aa010b7dc1a34c1b1c00bd094666d1e3.lua",
     [17625359962] = "https://api.luarmor.net/files/v3/loaders/4cc022ded41f6816ba0f7fb8c6dda44f.lua",
-    [537413528] = "https://api.luarmor.net/files/v3/loaders/ceffc1990de0fccb11999c53856ccd55.lua",
     [76558904092080] = "https://api.luarmor.net/files/v3/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
     [129009554587176] = "https://api.luarmor.net/files/v3/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
 }
