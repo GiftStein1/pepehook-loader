b83192e11828ff8efe4b736ae48b813a9a2b8576
Stein <73061040+GiftStein1@users.noreply.github.com>
2025-12-28 19:32:16 +0100

    Upgrade loader URLs to version 4
    Updated loader URLs from version 3 to version 4 for various PlaceIds.

--- loader.lua
+++ loader.lua
@@ -26,35 +26,35 @@ end
 
 local PlaceIds = {
     -- Rivals
-    [129604661913557] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
-    [133215910299950] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
-    [71874690745115] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
-    [117398147513099] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
-    [18126510175] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
-    [17625359962] = "https://api.luarmor.net/files/v3/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [129604661913557] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [133215910299950] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [71874690745115] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [117398147513099] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [18126510175] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
+    [17625359962] = "https://api.luarmor.net/files/v4/loaders/0dcb55226cdc9c555276db48ae35021f.lua",
     
     -- The Forge (World 1, 2, 3)
-    [76558904092080] = "https://api.luarmor.net/files/v3/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
-    [129009554587176] = "https://api.luarmor.net/files/v3/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
-    [131884594917121] = "https://api.luarmor.net/files/v3/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
+    [76558904092080] = "https://api.luarmor.net/files/v4/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
+    [129009554587176] = "https://api.luarmor.net/files/v4/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
+    [131884594917121] = "https://api.luarmor.net/files/v4/loaders/34c43d39431b1d16b9934f1122fd6f02.lua",
     
     -- Fish It
-    [121864768012064] = "https://api.luarmor.net/files/v3/loaders/b65dd9431e917a54cc84ed933242a95c.lua",
+    [121864768012064] = "https://api.luarmor.net/files/v4/loaders/b65dd9431e917a54cc84ed933242a95c.lua",
     
     -- Fisch
-    [17800150007] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [70451556031302] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [70527388434806] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [72907489978215] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [85537329004957] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [111674521372264] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [106011698424775] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [105044186406168] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [99519129453387] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [93179100126310] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [112780263016678] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [128543442402268] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
-    [131716211654599] = "https://api.luarmor.net/files/v3/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [17800150007] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [70451556031302] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [70527388434806] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [72907489978215] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [85537329004957] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [111674521372264] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [106011698424775] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [105044186406168] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [99519129453387] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [93179100126310] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [112780263016678] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [128543442402268] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
+    [131716211654599] = "https://api.luarmor.net/files/v4/loaders/f1e4e99ff97a9037ec469416c3333172.lua",
 }
 
 local currentPlaceId = game.PlaceId
